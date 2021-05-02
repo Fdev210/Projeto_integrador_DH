b9a2d074bdf2cb3a9ca976bdd8fd5e269f23664b
Fabiano SantAnna <vsantanna.fabiano@gmail.com>
2021-05-02 15:50:37 -0300

    Validador inserido com sucesso !

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const session = require('express-session');
 const router = express.Router();
+const validador = require('../middlewares/Validador');
 
 
 const CadastroController = require('../controller/CadastroController');
 
 /* GET home page. */
 // router.get('/', CadastroController.index)
-router.post('/', CadastroController.create)
+router.post('/', validador, CadastroController.create)
 router.get('/', CadastroController.read)
-router.put('/:id', CadastroController.update)
+router.put('/:id', validador, CadastroController.update)
 router.delete('/:id',  CadastroController.delete)
 router.get('/login', CadastroController.logged)
 
