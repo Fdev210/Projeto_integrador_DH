c29aa921599415043627bb99381b825e612a322d
murilosilverio78 <murilo.silverio@hotmail.com>
2021-05-15 19:25:32 -0300

    Header/Footer ajustados

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -9,7 +9,7 @@ const CadastroController = require('../controller/CadastroController');
 /* GET home page. */
 // router.get('/', CadastroController.index)
 router.post('/', validador, CadastroController.create)
-router.get('/', CadastroController.read)
+router.get('/', CadastroController.index)
 router.put('/:id', validador, CadastroController.update)
 router.delete('/:id',  CadastroController.delete)
 router.get('/login', CadastroController.logged)
