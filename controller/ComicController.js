994294c9d96562fa73275963f85cdb1bf5fd0a04
Fabiano SantAnna <vsantanna.fabiano@gmail.com>
2021-05-29 12:07:23 -0300

    transfere fileSystem em ComicController.storeComic para ComicService.createComic

--- controller/ComicController.js
+++ controller/ComicController.js
@@ -10,21 +10,7 @@ const ComicController = {
         
         const { filename } = req.file;
         
-        fs.readFile(comicsdb, 'utf-8', (err, comicsList) => {
-            if(err) throw err;
-        
-            const comic = ComicService.createComic(filename)
-
-            let comicArray = JSON.parse(comicsList)
-            comicArray.push(comic)
-
-            const insertComic = JSON.stringify(comicArray, null, 2)
-
-            fs.writeFile(comicsdb, insertComic, err => {
-                if(err) throw err;
-                console.log('Quadrinho inserido com sucesso')
-            });
-        });
+        ComicService.createComic(filename)
 
         res.json({
             url: `localhost:3000/uploads/${filename}`
--- services/ComicService.js
+++ services/ComicService.js
@@ -7,6 +7,15 @@ const { v4: uuidv4 } = require('uuid');
 const ComicService = {
     createComic: (filename) => {
         const newComic  = new ComicModel(uuidv4(), filename);
+
+        let comicList = fs.readFileSync(comicsdb, {encoding : 'utf-8'})
+        comicList = JSON.parse(comicList)
+
+        comicList.push(newComic)
+        const insertComic = JSON.stringify(comicList, null, 2)
+
+        fs.writeFileSync(comicsdb, insertComic, {encoding : 'utf-8'})
+
         return newComic
     },
 
