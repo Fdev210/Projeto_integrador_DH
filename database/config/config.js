d75db0da603e8df7818d658d6161d2c98bfb0df4
Fabiano SantAnna <vsantanna.fabiano@gmail.com>
2021-06-05 18:01:41 -0300

    database\config: dh_pi

--- database/config/config.js
+++ database/config/config.js
@@ -1,18 +1,9 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "",
-    "database": "pi",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
     "password": null,
-    "database": "database_test",
+    "database": "dh_pi",
     "host": "127.0.0.1",
     "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
+  }
 }
