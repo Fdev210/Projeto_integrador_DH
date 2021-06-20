1d654a70a28c11ab636dbd8a7fed1bd440624293
murilosilverio78 <murilo.silverio@hotmail.com>
2021-06-20 17:30:56 -0300

    bcrypt no seed

--- database/seeders/20210605221050-cliente-mestre.js
+++ database/seeders/20210605221050-cliente-mestre.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const bcrypt = require('bcryptjs')
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('clientes', [{  
       nome: 'Ivens Müller',
       email: 'ivensmuller@teste.com',
       telefone: '99999-9999',
-      senha_hash: '123456',
+      senha_hash: bcrypt.hashSync('123456', 12),
       data_nascimento: new Date(2000, 2, 21),
       createdAt: new Date(),
       updatedAt: new Date(),
@@ -14,7 +16,7 @@ module.exports = {
       nome: 'Bruno Yuri Alves',
       email: 'yuribruno@teste.com',
       telefone: '99999-9999',
-      senha_hash: '123456',
+      senha_hash: bcrypt.hashSync('123456', 12),
       data_nascimento: new Date(2000, 3, 22),
       createdAt: new Date(),
       updatedAt: new Date(),
@@ -22,7 +24,7 @@ module.exports = {
       nome: 'Nykolle Mallone',
       email: 'nykmallone@teste.com',
       telefone: '99999-9999',
-      senha_hash: '123456',
+      senha_hash: bcrypt.hashSync('123456', 12),
       data_nascimento: new Date(2000, 4, 10),
       createdAt: new Date(),
       updatedAt: new Date(),
